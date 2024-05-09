075aa9bcdb8832b572432c1dbf24d0aeb75a844c
wesdell <wesdell17@gmail.com>
2024-05-09 12:03:58 -0500

    Add methods to use and leave an urinal.

--- src/Urinal.java
+++ src/Urinal.java
@@ -1,3 +1,25 @@
 public class Urinal {
     private boolean available;
+
+    public Urinal(){
+        this.available = true;
+    }
+
+    public void use(int toiletNumber) {
+        if (!this.isAvailable()) {
+            System.out.println("El banio numero " + toiletNumber + " no esta disponible");
+            return;
+        }
+        this.available = false;
+        System.out.println("Usando el banio numero " + toiletNumber);
+    }
+
+    public void leave(int toiletNumber) {
+        this.available = true;
+        System.out.println("Saliendo del banio numero " + toiletNumber);
+    }
+
+    public boolean isAvailable() {
+        return this.available;
+    }
 }

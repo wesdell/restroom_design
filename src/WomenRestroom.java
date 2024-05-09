0648b5419c4483ceef869fe97dca30065a3c6d1f
wesdell <wesdell17@gmail.com>
2024-05-09 09:21:49 -0500

    Create WomenRestroom constructor and methods to get and to modify attributes.

--- src/WomenRestroom.java
+++ src/WomenRestroom.java
@@ -1,4 +1,21 @@
 public class WomenRestroom {
   private Toilet[] toilets;
   private AccessibleToilet accessibleToilet;
+
+  public WomenRestroom() {
+    this.accessibleToilet = new AccessibleToilet();
+    this.toilets = new Toilet[4];
+    this.toilets[0] = new Toilet();
+    this.toilets[1] = new Toilet();
+    this.toilets[2] = new Toilet();
+    this.toilets[3] = new Toilet();
+  }
+
+  public Toilet chooseToilet(int toiletNumber) {
+    return this.toilets[toiletNumber];
+  }
+
+  public  AccessibleToilet chooseAccessibleToilet() {
+    return accessibleToilet;
+  }
 }

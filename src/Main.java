e6572ef4c6c4a4d7a528c7e1305c2465844bd735
wesdell <wesdell17@gmail.com>
2024-05-09 12:04:23 -0500

    Test code.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
     Building fisRestroomFloor5 = new Building();
     WomenRestroom womenRestroom = fisRestroomFloor5.getWomenRestroom();
     Toilet toilet = womenRestroom.chooseToilet(1);
+    toilet.openDoor();
+    toilet.openSeatCover();
     toilet.use(1);
+    toilet.closeSeatCover();
+    toilet.closeDoor();
+    toilet.leave(1);
+    Toilet toilet2 = womenRestroom.chooseToilet(1);
+    toilet2.use(1);
   }
 }

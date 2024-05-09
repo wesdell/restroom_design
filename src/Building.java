ea6b372b6fae9be27f20c77e563c4ac24fdf6b5a
wesdell <wesdell17@gmail.com>
2024-05-09 09:20:26 -0500

    Create Building constructor and methods to get attrributes.

--- src/Building.java
+++ src/Building.java
@@ -1,4 +1,17 @@
 public class Building {
   private WomenRestroom womenRestroom;
   private MenRestroom menRestroom;
+
+  public Building() {
+    this.womenRestroom = new WomenRestroom();
+    this.menRestroom = new MenRestroom();
+  }
+
+  public WomenRestroom getWomenRestroom() {
+    return womenRestroom;
+  }
+
+  public MenRestroom getMenRestroom() {
+    return menRestroom;
+  }
 }

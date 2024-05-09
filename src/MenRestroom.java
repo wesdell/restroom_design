542351af0884b76ad605cd5fb33dbc4ba43303f3
wesdell <wesdell17@gmail.com>
2024-05-09 12:01:47 -0500

    Add methods to get a toilet by id and to get an accessible toilet.

--- src/MenRestroom.java
+++ src/MenRestroom.java
@@ -1,5 +1,30 @@
 public class MenRestroom {
+  public static final int TOILETS_AMOUNT = 4;
+  public static final int URINALS_AMOUNT = 4;
+
   private Toilet[] toilets;
   private AccessibleToilet accessibleToilet;
   private Urinal[] urinals;
+
+  public MenRestroom() {
+    this.accessibleToilet = new AccessibleToilet();
+    this.toilets = new Toilet[TOILETS_AMOUNT];
+    this.toilets[0] = new Toilet();
+    this.toilets[1] = new Toilet();
+    this.toilets[2] = new Toilet();
+    this.toilets[3] = new Toilet();
+    this.urinals = new Urinal[URINALS_AMOUNT];
+    this.urinals[0] = new Urinal();
+    this.urinals[1] = new Urinal();
+    this.urinals[2] = new Urinal();
+    this.urinals[3] = new Urinal();
+  }
+
+  public Toilet chooseToilet(int toiletNumber) {
+    return this.toilets[toiletNumber];
+  }
+
+  public  AccessibleToilet chooseAccessibleToilet() {
+    return accessibleToilet;
+  }
 }

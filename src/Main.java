398ab68934bd865ed3fb423f10e948770b298e81
wesdell <wesdell17@gmail.com>
2024-05-09 09:22:04 -0500

    Test code.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Building fisRestroomFloor5 = new Building();
+    WomenRestroom womenRestroom = fisRestroomFloor5.getWomenRestroom();
+    Toilet toilet = womenRestroom.chooseToilet(1);
+    toilet.use(1);
   }
 }

30c46df9a7cabad2c96352bdfa802153f50bedf2
wesdell <wesdell17@gmail.com>
2024-05-09 09:21:10 -0500

    New methods to access and to modify attributes data.

--- src/Toilet.java
+++ src/Toilet.java
@@ -3,4 +3,16 @@ public class Toilet {
     private boolean toiletPaper;
     private boolean openSeatCover;
     private boolean openDoor;
+
+    public void use(int toiletNumber) {
+        if (!this.isAvailable()) {
+            System.out.println("El banio numero " + toiletNumber + " no esta disponible");
+            return;
+        }
+        System.out.println("Usando el banio numero " + toiletNumber);
+    }
+
+    public boolean isAvailable() {
+        return this.available;
+    }
 }

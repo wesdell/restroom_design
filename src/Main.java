40b0de43463f129d0bd142a747564fb3c7f80bb7
wesdell <wesdell17@gmail.com>
2024-05-10 14:53:57 -0500

    Test with multiple users using urinals.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
   public static void main(String[] args) {
     Building building = new Building();
-    Person person = new Person(Genre.MAN, Disability.NO);
-    person.useUrinal(building);
+    Person man1 = new Person(Genre.MAN, Disability.NO);
+    Person man2 = new Person(Genre.MAN, Disability.NO);
+    Person man3 = new Person(Genre.MAN, Disability.NO);
+    Person man4 = new Person(Genre.MAN, Disability.NO);
+    Person man5 = new Person(Genre.MAN, Disability.NO);
+    Person woman = new Person(Genre.WOMAN, Disability.NO);
+    man1.useRestroom(building);
+    man2.useRestroom(building);
+    man3.useRestroom(building);
+    man4.useRestroom(building);
+    man5.useRestroom(building);
+    man3.leaveCleanRestroom();
+    man5.useRestroom(building);
+    woman.useRestroom(building);
     System.out.println(building.getRestroomsAvailable());
   }
 }

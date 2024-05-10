b21b4ecb9fe8728596dfebb3d6258c8a2ff38b98
Wesdell <wesdell17@gmail.com>
2024-05-09 21:24:06 -0500

    New methods to use and to read Toilet data.

--- src/Toilet.java
+++ src/Toilet.java
@@ -1,7 +1,17 @@
 public class Toilet {
-//    public boolean isAvailableClean() {
-//    }
-//
-//    public void use() {
-//    }
+    private boolean available;
+    private boolean clean;
+
+    public boolean isAvailableClean() {
+        return this.available && this.clean;
+    }
+
+    public void use() {
+        this.available = false;
+    }
+
+    @Override
+    public String toString() {
+        return this.available ? "Available" : "Not Available";
+    }
 }

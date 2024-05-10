5695942ed19a72f6afd646326fc7d7b5d98200cf
wesdell <wesdell17@gmail.com>
2024-05-10 14:58:27 -0500

    Add method to use and to leave a toilet.

--- src/Toilet.java
+++ src/Toilet.java
@@ -2,16 +2,35 @@ public class Toilet {
     private boolean available;
     private boolean clean;
 
-    public boolean isAvailableClean() {
-        return this.available && this.clean;
+    public Toilet() {
+        this.available = true;
+        this.clean = true;
     }
 
     public void use() {
         this.available = false;
+        this.clean = false;
+    }
+
+    public void leaveClean() {
+        this.available = true;
+        this.clean = true;
+    }
+
+    public boolean isAvailableClean() {
+        return this.available && this.clean;
+    }
+
+    public boolean isAvailable() {
+        return this.available;
+    }
+
+    public String printAvailability() {
+        return this.available ? "Available" : "Not Available";
     }
 
     @Override
     public String toString() {
-        return this.available ? "Available" : "Not Available";
+        return getClass().getName() + " (" + printAvailability() + ")";
     }
 }

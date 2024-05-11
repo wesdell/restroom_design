351263436a81de49897cf787b96cdce47062078b
Wesdell <wesdell17@gmail.com>
2024-05-10 19:21:40 -0500

    Test code.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 /*
-* Single Responsibility ->  Un metodo es encargado de realizar una sola tarea, es decir,
-* realizar una unica operacion.
-* Los siguientes conceptos forman parte del disenio.
-* Cohesion -> Relacion entre elementos.
-* Acoplamiento -> Dependencia con otros elementos.
-* */
+ * Single Responsibility ->  Un metodo es encargado de realizar una sola tarea, es decir,
+ * realizar una unica operacion.
+ * Los siguientes conceptos forman parte del disenio.
+ * Cohesion -> Relacion entre elementos.
+ * Acoplamiento -> Dependencia con otros elementos.
+ * */
 
 public class Main {
   public static void main(String[] args) {
